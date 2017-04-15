c2a567b0254eb91a08fdf675ba63b039ba19222c
johnmcdnl <john.mcdonnell439@gmail.com>
2017-04-15 13:19:33 +0100

    updates

--- main.go
+++ main.go
@@ -1,33 +1,14 @@
-//package main
-//
-//import (
-//	"fmt"
-//	"github.com/johnmcdnl/xrun/xrun"
-//	"os"
-//)
-
-//func main() {
-//	fmt.Println("XRUN v0.1")
-//
-//	new(xrun.Runner).Build()
-//}
-//
-
 package main
 
 import (
-"os"
-"github.com/ajstarks/svgo"
+	"fmt"
+	"github.com/johnmcdnl/xrun/xrun"
 )
+
 func main() {
-	width := 500
-	height := 500
-	canvas := svg.New(os.Stdout)
-	canvas.Start(width, height)
-	canvas.Rect(0, 0, width, height)
-	canvas.Ellipse(width/2, height, width/2,height/3,"fill:rgb(44,77,232)")
-	canvas.Text(width/2, height/2, "Hello, World", "fill:white;font-size:48pt;text-anchor:middle")
-	canvas.End()
+	fmt.Println("XRUN v0.1")
 
-
+	new(xrun.Runner).Build()
 }
+
+
